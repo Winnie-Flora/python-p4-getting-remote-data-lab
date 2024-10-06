820c4c3d5e6e6199e485be8df7d2f69d3cd9d6c5
Winnie-Flora <contentcreatorwinnie@gmail.com>
2024-10-06 15:40:55 +0300

    Get Remote Data

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,22 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        requester = requests.get(self.url)
+
+        return requester.content
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        return json.loads(response_body)
+
+if __name__ == "__main__":
+    url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+    requester = GetRequester(url)
+
+    try:
+        json_data = requester.load_json()
+        print(json_data)
+    except requests.exceptions.HTTPError as e:
+        print(f"HTTP error occurred: {e}")
+    except json.JSONDecodeError as e:
+        print(f"Error decoding JSON: {e}")
